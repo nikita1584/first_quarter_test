8df7d6ed48cb34e3c4081f5595270825f1ce2411
«Kozlov <nikita1584@gmail.com>
2022-06-16 17:37:33 +0300

    Added text of programm Program.cs and .gitignore

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача состоит в написании текста программы, которая из имеющегося массива
+// формирует массив из строк, длина которых меньше либо равна 3 символа.
+// { "hello", "123", "2", "world", ":-)" } => { "123", "2", ":-)" }
+
+string[] initialArray = new string[] { "hello", "123", "2", "world", ":-)" };
+string[] resultArray = new string[initialArray.Length];
+const int maxSize = 3;
+byte j = 0;
+
+for (byte i = 0; i < initialArray.Length; i++)
+{
+    if (initialArray[i].Length <= maxSize)
+        resultArray[j++] = initialArray[i];
+}
